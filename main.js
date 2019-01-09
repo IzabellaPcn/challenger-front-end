8f033bf224dfae8c17864a907d4e41136ba54d17
IzabellaPris <izabellapriscylla@hotmail.com>
2019-01-09 19:42:45 -0200

    ajustes da tela baterias

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+var data;
+var historic;
 const Http = new XMLHttpRequest();
-const url = 'json/batteries.json';
+const url = 'http://estagio-app.herokuapp.com/api/historic/battery/';
 Http.open("GET", url);
 Http.setRequestHeader("Content-type", "application/json");
 Http.send();
@@ -7,24 +9,23 @@ Http.onreadystatechange = function(e) {
 	if (Http.readyState == XMLHttpRequest.DONE ) {
        if (Http.status == 200) {
 			let content = Http.responseText;
-			let data = JSON.parse(content);
+			data = JSON.parse(content);
 			data = data["data"];                                                 
 			let div = document.getElementById("bloco");
 			for (var i = 0; i < data.length; i++) {
 				let htmlFinal = "";
-				let request = new XMLHttpRequest(); //http://estagio-app.herokuapp.com/api/historic/battery/8118869
-				request.open("GET", "json/" + data[i]["serial_number"] + ".json");
+				let request = new XMLHttpRequest();
+				request.open("GET", "http://estagio-app.herokuapp.com/api/historic/battery/" + data[i]["serial_number"]);
 				request.setRequestHeader("Content-type", "application/json");
 				request.order = i;
 				request.send(); 
 				request.onreadystatechange = function(e) {
 					if (request.readyState == XMLHttpRequest.DONE ) {
        					if (request.status == 200) {
-       						console.log(data)
-							let historic = JSON.parse(request.responseText);
+       						historic = JSON.parse(request.responseText);
 							historic = historic["data"];    
 							htmlFinal += "<div class=\"bateria\">";
-							htmlFinal += "<p><span>" + data[this.order]["battery_name"] + "</span></p>";
+							htmlFinal += "<p><span class=\"destaque\">" + data[this.order]["battery_name"] + "</span></p>";
 							htmlFinal += "<p><span>Data</span><span>" + historic[historic.length-1]["timestamp_upload"] + "</span></p>";
 							let estadoTemp = "temperatura ";
 							if (data[this.order]["last_temperature"] < 3) {
@@ -43,7 +44,8 @@ Http.onreadystatechange = function(e) {
 								estadoCarg += "high";
 							}
 							htmlFinal += "<p class=\"" + estadoTemp + "\"><span>Temperatura</span><span>" + data[this.order]["last_temperature"].toFixed(2) + "</span></p>";
-							htmlFinal += "<p class=\"" + estadoCarg + "\"><span>Carga</span><span>" + data[this.order]["last_charge"].toFixed(2) + "</span></p></div>";
+							htmlFinal += "<p class=\"" + estadoCarg + "\"><span>Carga</span><span>" + data[this.order]["last_charge"].toFixed(2) + "</span></p>";
+							htmlFinal += "<p class=\"linkHistorico\" ><a class=\"texto\" href=\"\"><span>Histórico da bateria</span></a></p></div>";
 							div.innerHTML += htmlFinal;
 						}
 					}
